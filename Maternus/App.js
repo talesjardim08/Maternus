2134778170d5302e77e3a6691b6ce67e7d5d0ab3
Tales de Souza <talesjardim08@gmail.com>
2025-09-01 06:49:21 +0000

    Correção login.js

--- Maternus/App.js
+++ Maternus/App.js
@@ -2,7 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginScreen from'./screens/LoginScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import LoginScreen from'./screens/login';
 import CadScreen from'./screens/cadastro';
 
 const Stack = createNativeStackNavigator();
